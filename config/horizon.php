675ddad6b8e5119dc8e886ea48fd69ccc9aaa6e5
tom-burness <tomburness@proton.me>
2025-02-04 20:02:05 +0000

    build: update horizon config

--- config/horizon.php
+++ config/horizon.php
@@ -56,7 +56,7 @@ return [
 
     'prefix' => env(
         'HORIZON_PREFIX',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_horizon:'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_horizon:'
     ),
 
     /*
@@ -206,7 +206,7 @@ return [
 
         'local' => [
             'supervisor-1' => [
-                'maxProcesses' => 3,
+                'maxProcesses' => 10,
             ],
         ],
     ],
